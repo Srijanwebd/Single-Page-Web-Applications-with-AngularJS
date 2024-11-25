2fb9ff0e1ff06a7800ef006976633c46afb6e32d
SRIJAN <sharmasrijan82@gmail.com>
2024-11-25 19:54:36 +0530

    Update app.js

--- module 1/app.js
+++ module 1/app.js
@@ -1,36 +1,33 @@
-// app.js
-(function () {
-  "use strict";
+(function() {
+    'use strict';
 
-  angular
-    .module("LunchCheck", [])
-    .controller("LunchCheckController", LunchCheckController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ["$scope"];
+    LunchCheckController.$inject = ['$scope'];
 
-  function LunchCheckController($scope) {
-    $scope.lunchItems = "";
-    $scope.message = "";
+    function LunchCheckController($scope) {
+        $scope.dishes = '';
+        $scope.message = '';
+        $scope.checked = false;
 
-    $scope.checkIfTooMuch = function () {
-      if (!$scope.lunchItems.trim()) {
-        $scope.message = "Please enter data first";
-        $scope.messageClass = "red";
-        $scope.inputClass = "red";
-      } else {
-        var items = $scope.lunchItems.split(",").filter(function (item) {
-          return item.trim() !== "";
-        });
-        if (items.length <= 3) {
-          $scope.message = "Enjoy!";
-          $scope.messageClass = "green";
-          $scope.inputClass = "green";
-        } else {
-          $scope.message = "Too much!";
-          $scope.messageClass = "green";
-          $scope.inputClass = "green";
-        }
-      }
-    };
-  }
+        $scope.checkLunch = function() {
+            if ($scope.dishes.trim().length === 0) {
+                $scope.empty = true;
+            } else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var arrayDishes = $scope.dishes.split(',');
+                var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+                    return v.trim() !== '';
+                });
+
+                if (arrayDishesWithoutEmptys.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
+            }
+        };
+    }
 })();
